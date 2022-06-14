8d4f69a495196091728c46aecf929c06f38aa040
Rokuazery <Rokuro81@gmail.com>
2022-06-14 08:51:49 +0700

    coin flip init

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,100 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+// #[macro_use]
+// extern crate lazy_static;
+
+use rand::Rng; // 0.8.0
+use std::cell::RefCell;
+
+const COIN_FACES: [&str; 2] = ["HEADS", "TAILS"];
+// static mut FLIP_HISOTRY: Vec<String> = Vec::new();
+thread_local!(static HEADS_AMOUNT: RefCell<u32> = RefCell::new(0));
+thread_local!(static TAILS_AMOUNT: RefCell<u32> = RefCell::new(0));
+thread_local!(static FLIP_HISOTRY: RefCell<Vec<String>> = RefCell::new(vec![]));
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            flip_coin,
+            get_flip_history,
+            get_heads_amount,
+            get_tails_amount,
+            clear_flip_history,
+            open_github_url
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
+
+#[tauri::command]
+fn flip_coin() -> String {
+    let index = rand::thread_rng().gen_range(0..2);
+    let face = COIN_FACES[index].to_string();
+
+    FLIP_HISOTRY.with(|history| {
+        let coin_history = format!("[{}] {}", { history.borrow().len() + 1 }, face);
+        history.borrow_mut().push(coin_history);
+    });
+
+    return face;
+}
+
+#[tauri::command]
+fn get_flip_history() -> Vec<String> {
+    FLIP_HISOTRY.with(|history| {
+        let history_vec = &history.borrow();
+
+        if history_vec.len() > 0 {
+            let _result = &history_vec[history_vec.len() - 1];
+
+            if _result.contains(&COIN_FACES[0]) {
+                HEADS_AMOUNT.with(|amount| {
+                    *amount.borrow_mut() += 1;
+                });
+            } else {
+                TAILS_AMOUNT.with(|amount| {
+                    *amount.borrow_mut() += 1;
+                });
+            }
+        }
+
+        return history.borrow().to_vec();
+    })
+}
+
+#[tauri::command]
+fn clear_flip_history() {
+    FLIP_HISOTRY.with(|history| {
+        history.borrow_mut().clear();
+    });
+    TAILS_AMOUNT.with(|amount| {
+        *amount.borrow_mut() = 0;
+    });
+    HEADS_AMOUNT.with(|amount| {
+        *amount.borrow_mut() = 0;
+    });
+}
+
+#[tauri::command]
+fn open_github_url() {
+    open::that("https://github.com/Rokuazery").unwrap();
+}
+
+#[tauri::command]
+fn get_heads_amount() -> u32 {
+    HEADS_AMOUNT.with(|h| {
+        return *h.borrow();
+    })
+}
+
+#[tauri::command]
+fn get_tails_amount() -> u32 {
+    TAILS_AMOUNT.with(|t| {
+        return *t.borrow();
+    })
+}
+
+//thread_local!(static FLIP_HISOTRY: RefCell<Vec<String>> = RefCell::new(vec![]);
--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,95 @@
+import { invoke } from '@tauri-apps/api/tauri'
 import { useState } from 'react'
-import logo from './logo.svg'
+import $ from 'jquery';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import head from './assets/head.svg'
+import tail from './assets/tail.svg'
+import click from './assets/click.mp3'
+import coin_1 from './assets/coin_1.mp3'
+import coin_2 from './assets/coin_2.mp3'
 
+function App() {
+  const [coinClass, setCoinClass] = useState(0)
+  const [coinFace, setCoinFace] = useState("Flip the coin")
+  const [isFlipping, setIsFlipping] = useState(false)
+  const [flipCount, setFlipCount] = useState(0)
+  const [headsCount, setHeadsCount] = useState(0)
+  const [tailsCount, setTailsCount] = useState(0)
+  const [buttonState, setButtonState] = useState(null);
+  const [flips, setFlips] = useState([]);
+
+  let buttonAudio = new Audio(click)
+  let coinAudio_1 = new Audio(coin_1)
+  let coinAudio_2 = new Audio(coin_2)
+
+ const scrollSmoothlyToBottom = (id) => {
+  const element = $(`#${id}`); 
+  element.animate({
+     scrollTop: element.prop("scrollHeight")
+  }, 1000);
+}
+
+const Reset = () => {
+  buttonAudio.play()
+  setFlipCount(0)
+  setHeadsCount(0)
+  setTailsCount(0)
+  invoke('clear_flip_history')
+  invoke('get_flip_history').then((history) =>{ setFlips(history)})
+}
+
+const Flip = () => {
+  if(!isFlipping)
+  {
+    coinAudio_1.play()
+    setCoinClass(null)
+    setIsFlipping(true)
+    setButtonState("disabled")
+    setCoinFace("Flipping...")
+    
+    setFlipCount((flipCount) => flipCount + 1)
+    invoke('flip_coin').then((coinResult) => {
+    setCoinClass(coinResult)
+      
+    setTimeout(() => { // wait for the animation to finish
+      coinAudio_2.play();
+      setButtonState(null)
+      setIsFlipping(false)
+      setCoinFace(coinResult)
+      invoke('get_flip_history').then((history) =>{setFlips(history)})
+      invoke('get_heads_amount').then((amount) => {setHeadsCount(amount)})
+      invoke('get_tails_amount').then((amount) => {setTailsCount(amount)})
+      scrollSmoothlyToBottom('history')
+      }, 3000)
+    })
+  }
+}
   return (
-    <div className="App">
+    <body>
+      <div id="tracker">
+        <p id="tracker-p">flipped count: {flipCount} | HEADS: {headsCount} | TAILS: {tailsCount}</p>
+        <div className='history-container' id='history'>
+          {flips.map((flip) => (<p className='history-p'>{flip}</p>))}
+        </div>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <div id="coin" className={coinClass}>
+            <div className="side-heads"><img src={head}></img></div>
+            <div className="side-tails"><img src={tail}></img></div>
+          </div>
+          <p>{coinFace}</p>
+          <p>
+            <button type="button" onClick={Flip} disabled={buttonState}>
+              Flip it
+            </button>
+          </p>
+          <p className='Author' onClick={()=>{invoke('open_github_url')}}>Rokuazery's Github ❤️</p>
+          <button id='reset' type="button" onClick={Reset} disabled={buttonState}>
+              <span>Reset </span>
           </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+        </header>
+    </body>
   )
 }
 
